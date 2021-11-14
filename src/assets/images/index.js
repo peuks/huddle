50e0d05e39631d33d3318b1ac287ac7e14823356
peuks <vanmakdavid.lwg@gmail.com>
2021-11-14 11:13:45 +0100

    INIT: Initial Commit

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,1 @@
-export { default as Calculator } from "./icon-calculator.svg";
-export { default as Karma } from "./icon-karma.svg";
-export { default as Supervisor } from "./icon-supervisor.svg";
-export { default as TeamBuilder } from "./icon-team-builder.svg";
+// export { default as Calculator } from "./icon-calculator.svg";
